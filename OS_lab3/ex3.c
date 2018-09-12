4c7936938a47dddf816f4442d0f9e369c5296b1f
iambackend <iambackend@gmail.com>
2018-09-12 23:24:51 +0300

    Lab 3 ex1 done

--- OS_lab3/ex3.c
+++ OS_lab3/ex3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node_t node;
 
@@ -12,24 +13,29 @@ typedef struct list_t {
 } list;
 
 void print_list(list *l){
+	printf("OK\n");
 	for(node *i = l->head; i->next != 0; i = i->next){ 
-		printf("%d ", *i);
+		printf("%d ", i -> value);
 	}
 	printf("\n");
 }
 
 void insert_node(list *l, int n, int value){ // n - number of node, after wich we should insert new node with value value
-	node nod = l -> head;
+	if(l -> head == 0){
+		l -> head = (node*) malloc(sizeof(node));
+		return;
+	}
+	node *nod = l -> head;
 	for(int i = 0; i < n; i++){
 	     nod = nod -> next;
 	}
-	new_node = (node*) malloc(sizeof(node));
-	new_node = {value, nod->next};
+	node *new_node = (node*) malloc(sizeof(node));
+	new_node = (node *){value, nod->next};
 	nod -> next = new_node;
 }
 
-e delete_node(list *l, int n){ // delete node with number n from list 
-	node nod = l -> head;
+void delete_node(list *l, int n){ // delete node with number n from list 
+	node *nod = l -> head;
 	for(int i = 0; i < n - 1; i++){
 		nod = nod -> next;
 	}
@@ -37,11 +43,22 @@ e delete_node(list *l, int n){ // delete node with number n from list
 		l -> head = l -> head -> next;
 	}
 	else{
-		node x = nod -> next -> next;
+		node *x = nod -> next -> next;
 		free(nod -> next);
 		nod -> next = x;
 	}
 }
 
 int main(){	
+	list *l;
+	printf("OK1\n");
+	l -> head = (node*)0;
+	printf("OK2\n");
+	print_list(l);
+	insert_node(l, 2, 0);
+	print_list(l);
+	insert_node(l, 5, 0);
+	print_list(l);
+	insert_node(l, 3, 1);
+	print_list(l);
 }
