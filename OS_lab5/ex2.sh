7dd057b657bae2b8ada1c38723280f237b31a568
iambackend <iambackend@gmail.com>
2018-09-20 00:14:46 +0300

    ex2 done  Please enter the commit message for your changes. Lines starting

--- OS_lab5/ex2.sh
+++ OS_lab5/ex2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-for i in {1..1000}
+for i in {1..3000}
 do
-	a=$(tail -n 1 "ex2.txt")
-	echo "$(($a + 1))" >> "ex2.txt"
+	while true; 
+	do 
+		if `ln ex2.txt ex2.txt.lock`; then
+			a=$(tail -n 1 "ex2.txt")
+			echo "$(($a + 1))" >> "ex2.txt"
+			rm ex2.txt.lock
+			break
+		fi
+	done
 done
