52df08f76c7a50ebdb4d5add6d16c51dd0c57c34
iambackend <iambackend@gmail.com>
2018-10-11 21:25:41 +0300

    ex2 fits requarement

--- week8/ex2.c
+++ week8/ex2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TEN_MB 30000000
+#define TEN_MB 10000000
 
 int main(){
-	for(int i = 0; i < 20; i++){
+	for(int i = 0; i < 10; i++){
 		int* a = malloc(TEN_MB);
 		memset(a, 0, TEN_MB);
 		sleep(1);
